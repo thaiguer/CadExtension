20092943c5b8702f1fa1f78ba85e174b66812f57
thaiguer <thaiguer.k@gmail.com>
2025-07-08 23:26:47 -0300

    adjust .net target version

--- Initialization.cs
+++ Initialization.cs
@@ -1,4 +1,8 @@
 ﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 
 namespace CadExtension;
 
@@ -6,7 +10,8 @@ internal class Initialization : IExtensionApplication
 {
     public void Initialize()
     {
-
+        Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+        ed.WriteMessage("It's alive!\n");
     }
 
     public void Terminate()
