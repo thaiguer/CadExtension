0861e93bdc769c2185a1723282b1fbc57de20ba1
thaiguer <thaiguer.k@gmail.com>
2025-07-08 23:09:09 -0300

    hello world

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+namespace CadExtension;
+
+public class Class1
+{
+    [CommandMethod("HELLO")]
+    public void HelloCommand()
+    {
+        // Get the AutoCAD editor.
+        Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+        // Display the "Hello, World!" message.
+        ed.WriteMessage("Hello, World!\n");
+    }
+}
---
+++ Initialization.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace CadExtension;
+
+internal class Initialization : IExtensionApplication
+{
+    public void Initialize()
+    {
+
+    }
+
+    public void Terminate()
+    {
+
+    }
+}
