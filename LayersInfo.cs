fe14c7368ee39c7eebb13b4694e358cc31a04f4a
thaiguer <thaiguer.k@gmail.com>
2025-07-09 22:50:12 -0300

    add layers info class

---
+++ LayersInfo.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace CadExtension;
+
+internal class LayersInfo : List<LayerInfo>
+{
+    internal void GetLayersState()
+    {
+        Document document = Application.DocumentManager.MdiActiveDocument;
+        Database database = document.Database;
+
+        List<LayerInfo> layers = new List<LayerInfo>();
+
+        using (Transaction transaction = database.TransactionManager.StartTransaction())
+        {
+            LayerTable layerTable = transaction.GetObject(database.LayerTableId, OpenMode.ForRead) as LayerTable;
+            foreach (ObjectId layerId in layerTable)
+            {
+                LayerTableRecord layerRecord = transaction.GetObject(layerId, OpenMode.ForRead) as LayerTableRecord;
+                Add(new LayerInfo(layerRecord.Name, layerRecord.IsOff == false));
+            }
+
+            transaction.Commit();
+        }
+    }
+
+    internal void UpdateLayersState()
+    {
+        Document document = Application.DocumentManager.MdiActiveDocument;
+        Database database = document.Database;
+
+        using (Transaction transaction = database.TransactionManager.StartTransaction())
+        {
+            LayerTable layerTable = transaction.GetObject(database.LayerTableId, OpenMode.ForRead) as LayerTable;
+
+            foreach (LayerInfo info in this)
+            {
+                if (layerTable.Has(info.LayerName))
+                {
+                    LayerTableRecord layerRecord = transaction.GetObject(layerTable[info.LayerName], OpenMode.ForWrite) as LayerTableRecord;
+                    layerRecord.IsOff = !info.IsOn;
+                }
+            }
+
+            transaction.Commit();
+        }
+    }
+}
