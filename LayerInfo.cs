c13aafae0ca37e7c8652a1e291ff308ab2d1e5df
thaiguer <thaiguer.k@gmail.com>
2025-07-09 22:51:35 -0300

    add layer info file

---
+++ LayerInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace CadExtension;
+
+internal class LayerInfo
+{
+    public string LayerName { get; }
+    public bool IsOn { get; set; }
+
+    public LayerInfo(string layerName, bool isOn)
+    {
+        LayerName = layerName;
+        IsOn = isOn;
+    }
+}
