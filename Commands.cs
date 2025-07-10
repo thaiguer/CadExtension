527738da4716a0fc6dbd4777ecdaefef0af8b389
thaiguer <thaiguer.k@gmail.com>
2025-07-09 23:03:27 -0300

    adjust for necessary commands

---
+++ Commands.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+namespace CadExtension;
+
+public class Commands
+{
+    [CommandMethod("VS_SAVE_CURRENT")]
+    public void SaveCurrentViewCommand()
+    {
+        Prompt.WriteNewLine("Save Current Command not implemented");
+    }
+
+    [CommandMethod("VS_REMOVE_SAVED")]
+    public void RemoveSavedViewsCommand()
+    {
+        Prompt.WriteNewLine("Remove saved views Command not implemented");
+    }
+
+    [CommandMethod("VS_RESTORE_SAVED")]
+    public void RestoreSavedViewCommand()
+    {
+        Prompt.WriteNewLine("Restore saved view Command not implemented");
+    }
+}
